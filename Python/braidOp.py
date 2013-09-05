eaac31b0024a89627e90564a15785c7933b054a6
mattrathbun <mrathbun@gmail.com>
2013-09-05 17:50:06 +0100

    Ported BraidOp except "isFlip" and "isReduction"

--- Python/braidOp.py
+++ Python/braidOp.py
@@ -9,6 +9,17 @@ down_moves = ['R2Down', 'M2Down']
 opNames = ['R2UpPlus', 'R2UpMinus', 'M2UpPlus', 'M2UpMinus', 'R3', 'M1Forward',
 	'M1Backward', 'S', 'R2Down', 'M2Down']
 
+def randomOp(upBias=1, horizontalBias=1, downBias=1):
+	randOp = random.choice(upBias*up_moves+downBias*down_moves+horizontalBias*horizontal_moves)
+	## We introduce here the possibility for bias. The 3 optional parameters default to 1,
+	## but can be attuned to multiply the likelihood of choosing an element from the
+	## corresponding list by exactly this factor.	
+	if randOp in R2_up_moves:
+		strand_index = 'undetermined'
+	else:
+		strand_index = None
+	return BraidOp(randOp, strand_index)
+
 class BraidOp(object):
 	def __init__(self, op, strand_index=None):
 		self.op = op
@@ -20,11 +31,20 @@ class BraidOp(object):
 		#### the allowed values depend on the braid index of the braid being operated on.
 		
 		
-	def randomOp(self, upBias=1, horizontalBias=1, downBias=1):
-		randOp = random.choice(upBias*up_moves+downBias*down_moves+horizontalBias*horizontal_moves)
-		## We introduce here the possibility for bias. The 3 optional parameters default to 1,
-		## but can be attuned to multiply the likelihood of choosing an element from the
-		## corresponding list by exactly this factor.	
-		return BraidOp(randOp)
+	def copy(self):
+		return BraidOp(self.op, self.strand_index)
+		
+	def apply(self, braid):
+		if self.op in R2_up_moves:
+			print 'I am here now.'
+			if self.strand_index == 'undetermined':
+				self.strand_index = random.randint(1, braid.braid_index() - 1)
+			getattr(braid, self.op)(self.strand_index)
+		else:
+			getattr(braid, self.op)()
+		
+	
+		
+	
 
 		
