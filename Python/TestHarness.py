93d639fb758ac210fb39003b5776b6e4927d978e
colingj <colingj@gmail.com>
2013-10-02 13:53:26 +0100

    a simple test program

---
+++ Python/TestHarness.py
@@ -0,0 +1,9 @@
+# okay, so in Python we can just write a sequence of commands
+# not like java where we have to wrap all this up in a class 
+
+import Braid
+print("test harness")
+braidInput = [1,2,5,4,6,3,2,1]
+b = Braid.Braid([1,2,5,4,3,2,1])
+b.print_it();
+
