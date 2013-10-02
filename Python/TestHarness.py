377f032f3dcd9d448a07769b4ba763eb521bf76d
colingj <colingj@gmail.com>
2013-10-02 15:36:08 +0100

    messing around with random and function mapping

--- Python/TestHarness.py
+++ Python/TestHarness.py
@@ -1,9 +1,33 @@
+# this is a place to mess around with creating objects etc.
+
 # okay, so in Python we can just write a sequence of commands
 # not like java where we have to wrap all this up in a class 
 
 import Braid
+import Population
+import random
+import numpy
+
 print("test harness")
 braidInput = [1,2,5,4,6,3,2,1]
 b = Braid.Braid([1,2,5,4,3,2,1])
 b.print_it();
+
+# p = Population.Population(20,15,10)
+
+
+######################################################################
+# the following is colin trying to understand how random numbers and
+# function application works
+
+longlist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for i in range(0,10):
+    print(random.choice(longlist));
+
+print(random.sample(longlist,3))
+
+print(map(lambda x:x*x, longlist))
+print(numpy.argmax(longlist))
+
+
 
