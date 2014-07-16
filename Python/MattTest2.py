52d497a540021d4a3db6af5f8ff896cbaffc0e27
Matt <Matt@dyn1189-93.wlan.ic.ac.uk>
2014-07-16 10:38:39 +0100

    Added test cases to MattTest2.py

--- Python/MattTest2.py
+++ Python/MattTest2.py
@@ -1,5 +1,6 @@
 import ADTLink
 
+print "################################################"
 K = ADTLink.ADTLink([6, 8, 2, 4], [-1, 1, -1, 1])
 
 print "Here's K before the R2UpPlus move:"
@@ -10,6 +11,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 1 to the right at [5, 4]"
 # L.R2UpPlus(1, 'r', [5, 4])
 L.R2Up(1, 'r', [5, 4])
+## L = ([10, 8, 12, -2, 4, 6], [-1, -1, 1, 1, -1, 1])
 print L.to_list()
 print "\n"
 
@@ -17,6 +19,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 2 to the right at [4, 5]"
 # L.R2UpPlus(2, 'r', [4, 5])
 L.R2Up(2, 'r', [4, 5])
+## L = ([10, 8, 12, -4, 2, 6], [-1, -1, 1, 1, -1, 1])
 print L.to_list()
 print "\n"
 
@@ -24,6 +27,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 2 to the right at [8, 7]"
 # L.R2UpPlus(2, 'r', [8, 7])
 L.R2Up(2, 'r', [8, 7])
+## L = ([8, 10, 12, 2, 6, -4], [-1, 1, 1, -1, 1, -1])
 print L.to_list()
 print "\n"
 
@@ -31,6 +35,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 2 to the right at [7, 8]"
 # L.R2UpPlus(2, 'r', [7, 8])
 L.R2Up(2, 'r', [7, 8])
+## L = ([6, 8, 2, 4], [-1, 1, -1, 1])
 print L.to_list()
 print "\n"
 
@@ -38,6 +43,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 6 to the right at [4, 3]"
 # L.R2UpPlus(6, 'r', [4, 3])
 L.R2Up(6, 'r', [4, 3])
+## L = ([8, 12, -10, 2, 4, 6], [-1, 1, -1, -1, 1, 1])
 print L.to_list()
 print "\n"
 
@@ -45,6 +51,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 6 to the right at [8, 1]"
 # L.R2UpPlus(6, 'r', [8, 1])
 L.R2Up(6, 'r', [8, 1])
+## L = ([6, 10, 2, 12, 4, -8], [-1, 1, -1, -1, 1, 1])
 print L.to_list()
 print "\n"
 
@@ -52,6 +59,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 3 to the right at [7, 8]"
 # L.R2UpPlus(3, 'r', [7, 8])
 L.R2Up(3, 'r', [7, 8])
+## L = ([8, 12, 10, 2, 6, -4], [-1, 1, 1, -1, 1, -1])
 print L.to_list()
 print "\n"
 
@@ -59,6 +67,7 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 7 to the left at [3, 2]"
 # L.R2UpPlus(7, 'l', [3, 2])
 L.R2Up(7, 'l', [3, 2])
+## L = ([8, -10, 12, 2, 6, 4], [-1, -1, 1, -1, 1, 1])
 print L.to_list()
 print "\n"
 
@@ -66,9 +75,66 @@ L = K.copy()
 print "Here's an attempt at an R2UpPlus move on 7 to the right at [3, 4]"
 # L.R2UpPlus(7, 'r', [3, 4])
 L.R2Up(7, 'r', [3, 4])
+## L = ([8, 12, -10, 2, 6, 4], [-1, 1, -1, -1, 1, 1])
 print L.to_list()
 print "\n"
 
+print "################################################"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 8 to the left at [6, 5]"
+L.R2Up(8, 'l', [6, 5])
+## L = ([8, 10, 2, -12, 4, 6], [-1, 1, -1, 1, 1, -1])
+print L.to_list()
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 8 to the right at [4, 3]"
+L.R2Up(8, 'r', [4, 3])
+## L = ([8, 10, -12, 2, 6, 4], [-1, 1, -1, -1, 1, 1])
+print L.to_list()
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 8 to the right at [3, 4]"
+L.R2Up(8, 'r', [3, 4])
+## L = ([6, 8, 2, 4], [-1, 1, -1, 1])
+print L.to_list()
+print "\n"
+
+#N = ADTLink.ADTLink([], [])
+
+#print N.R2Up(1, 'l', [1,2])
+#print N.to_list()
+## This returns an error.
+
+N = ADTLink.ADTLink([2], [-1])
+
+print "Here's N before the R2UpPlus move:"
+print N.to_list()
+print "\n"
+
+L = N.copy()
+print "Here's an attempt at an R2UpPlus move on 1 to the left at [1, 2]"
+L.R2Up(1, 'l', [1, 2])
+## L = ([2], [-1])
+print L.to_list()
+print "\n"
+
+L = N.copy()
+print "Here's an attempt at an R2UpPlus move on 1 to the left at [2, 1]"
+L.R2Up(1, 'l', [2, 1])
+## L = ([2], [-1])
+print L.to_list()
+print "\n"
+
+L = N.copy()
+print "Here's an attempt at an R2UpPlus move on 1 to the right at [1, 2]"
+print L.regions(1, 'r')
+L.R2Up(1, 'r', [1, 2])
+## L = ([4, 6, -2], [-1, -1, 1])
+print L.to_list()
+print "\n"
 
 
 # for i in range(1, 8):
