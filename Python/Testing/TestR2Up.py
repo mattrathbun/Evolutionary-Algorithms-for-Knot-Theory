8ecc381f3ed4f5fd78a7b5f99a404d7c42dfba31
Matt <Matt@ma-mrathbunlap.local>
2014-10-14 10:19:25 -0700

    Changed names of Test files to be more descriptive.

--- Python/Testing/TestR2Up.py
+++ Python/Testing/TestR2Up.py
@@ -73,4 +73,166 @@ print L.to_list()
 if L != ADT.ADT([-8, 10, -12, -14, -2, -4, 6], [-1, 1, -1, -1, -1, -1, 1]):
 	raise TypeError("This isn't the right result!")
 print '\n'
+
+print "################################################"
+K = ADT.ADT([6, 8, 2, 4], [-1, 1, -1, 1])
+
+print "Here's K before the R2UpPlus move:"
+print K.to_list()
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 1 to the right at [5, 4]"
+# L.R2UpPlus(1, 'r', [5, 4])
+L.R2Up(1, 'r', [5, 4])
+## L = ([10, 8, 12, -2, 4, 6], [-1, -1, 1, 1, -1, 1])
+print L.to_list()
+if L != ADT.ADT([10, 8, 12, -2, 4, 6], [-1, -1, 1, 1, -1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 2 to the right at [4, 5]"
+# L.R2UpPlus(2, 'r', [4, 5])
+L.R2Up(2, 'r', [4, 5])
+## L = ([10, 8, 12, -4, 2, 6], [-1, -1, 1, 1, -1, 1])
+print L.to_list()
+if L != ADT.ADT([10, 8, 12, -4, 2, 6], [-1, -1, 1, 1, -1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 2 to the right at [8, 7]"
+# L.R2UpPlus(2, 'r', [8, 7])
+L.R2Up(2, 'r', [8, 7])
+## L = ([8, 10, 12, 2, 6, -4], [-1, 1, 1, -1, 1, -1])
+print L.to_list()
+if L != ADT.ADT([8, 10, 12, 2, 6, -4], [-1, 1, 1, -1, 1, -1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 2 to the right at [7, 8]"
+# L.R2UpPlus(2, 'r', [7, 8])
+L.R2Up(2, 'r', [7, 8])
+## L = ([6, 8, 2, 4], [-1, 1, -1, 1])
+print L.to_list()
+if L != ADT.ADT([6, 8, 2, 4], [-1, 1, -1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 6 to the right at [4, 3]"
+# L.R2UpPlus(6, 'r', [4, 3])
+L.R2Up(6, 'r', [4, 3])
+## L = ([8, 12, -10, 2, 4, 6], [-1, 1, -1, -1, 1, 1])
+print L.to_list()
+if L != ADT.ADT([8, 12, -10, 2, 4, 6], [-1, 1, -1, -1, 1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 6 to the right at [8, 1]"
+# L.R2UpPlus(6, 'r', [8, 1])
+L.R2Up(6, 'r', [8, 1])
+## L = ([6, 10, 2, 12, 4, -8], [-1, 1, -1, -1, 1, 1])
+print L.to_list()
+if L != ADT.ADT([6, 10, 2, 12, 4, -8], [-1, 1, -1, -1, 1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 3 to the right at [7, 8]"
+# L.R2UpPlus(3, 'r', [7, 8])
+L.R2Up(3, 'r', [7, 8])
+## L = ([8, 12, 10, 2, 6, -4], [-1, 1, 1, -1, 1, -1])
+print L.to_list()
+if L != ADT.ADT([8, 12, 10, 2, 6, -4], [-1, 1, 1, -1, 1, -1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 7 to the left at [3, 2]"
+# L.R2UpPlus(7, 'l', [3, 2])
+L.R2Up(7, 'l', [3, 2])
+## L = ([8, -10, 12, 2, 6, 4], [-1, -1, 1, -1, 1, 1])
+print L.to_list()
+if L != ADT.ADT([8, -10, 12, 2, 6, 4], [-1, -1, 1, -1, 1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 7 to the right at [3, 4]"
+# L.R2UpPlus(7, 'r', [3, 4])
+L.R2Up(7, 'r', [3, 4])
+## L = ([8, 12, -10, 2, 6, 4], [-1, 1, -1, -1, 1, 1])
+print L.to_list()
+if L != ADT.ADT([8, 12, -10, 2, 6, 4], [-1, 1, -1, -1, 1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+print "################################################"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 8 to the left at [6, 5]"
+L.R2Up(8, 'l', [6, 5])
+## L = ([8, 10, 2, -12, 4, 6], [-1, 1, -1, 1, 1, -1])
+print L.to_list()
+if L != ADT.ADT([8, 10, 2, -12, 4, 6], [-1, 1, -1, 1, 1, -1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 8 to the right at [4, 3]"
+L.R2Up(8, 'r', [4, 3])
+## L = ([8, 10, -12, 2, 6, 4], [-1, 1, -1, -1, 1, 1])
+print L.to_list()
+if L != ADT.ADT([8, 10, -12, 2, 6, 4], [-1, 1, -1, -1, 1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = K.copy()
+print "Here's an attempt at an R2UpPlus move on 8 to the right at [3, 4]"
+L.R2Up(8, 'r', [3, 4])
+## L = ([6, 8, 2, 4], [-1, 1, -1, 1])
+print L.to_list()
+if L != ADT.ADT([6, 8, 2, 4], [-1, 1, -1, 1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+
+N = ADT.ADT([2], [-1])
+
+print "Here's N before the R2UpPlus move:"
+print N.to_list()
+print "\n"
+
+L = N.copy()
+print "Here's an attempt at an R2UpPlus move on 1 to the left at [1, 2]"
+L.R2Up(1, 'l', [1, 2])
+## L = ([2], [-1])
+print L.to_list()
+if L != ADT.ADT([2], [-1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = N.copy()
+print "Here's an attempt at an R2UpPlus move on 1 to the left at [2, 1]"
+L.R2Up(1, 'l', [2, 1])
+## L = ([2], [-1])
+print L.to_list()
+if L != ADT.ADT([2], [-1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
+L = N.copy()
+print "Here's an attempt at an R2UpPlus move on 1 to the right at [1, 2]"
+print L.regions(1, 'r')
+L.R2Up(1, 'r', [1, 2])
+## L = ([2], [-1])
+print L.to_list()
+if L != ADT.ADT([2], [-1]):
+	raise TypeError("Not the right result!")
+print "\n"
+
 	
