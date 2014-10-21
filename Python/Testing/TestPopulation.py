df1c01d76500b7e0a087b5f89f08ac6ac1aafe28
Nicholas Jackson <dr.nicholas.jackson@gmail.com>
2014-10-21 16:45:03 +0100

    Population -> ADTOpPopulation

--- Python/Testing/TestPopulation.py
+++ Python/Testing/TestPopulation.py
@@ -1,9 +1,9 @@
 import os, sys
 lib_path = os.path.abspath('../')
 sys.path.append(lib_path)
-import Population
+import ADTOpPopulation
 
-pop = Population.Population(10,10,5)
+pop = ADTOpPopulation.Population(10,10,5)
 
 ol = pop.toList()
 for l in ol:
