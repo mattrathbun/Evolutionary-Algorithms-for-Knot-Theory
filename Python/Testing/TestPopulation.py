2e8945208e85a6d33b0400e803cdf8da229ce0cb
Nicholas Jackson <dr.nicholas.jackson@gmail.com>
2014-10-21 17:34:55 +0100

    Added simple fitness function.

--- Python/Testing/TestPopulation.py
+++ Python/Testing/TestPopulation.py
@@ -3,9 +3,15 @@ lib_path = os.path.abspath('../')
 sys.path.append(lib_path)
 import ADTOpPopulation
 
+def fit(ol):
+    return ol.length()
+
 pop = ADTOpPopulation.Population(10,10,5)
+
+print "pop.size() = %d\n" % (pop.size())
 
 ol = pop.toList()
 for l in ol:
     print [op.toString() for op in l.toList()]
 
+pop.iterate(fit)
