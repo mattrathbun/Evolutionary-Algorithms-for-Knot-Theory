17b67a9f86880b59098c03f32b411216f7b4af2c
Nicholas Jackson <dr.nicholas.jackson@gmail.com>
2014-12-02 17:34:04 +0000

    Run autopep8 on TestInverses.py

--- Python/Testing/TestInverses.py
+++ Python/Testing/TestInverses.py
@@ -1,8 +1,11 @@
-import os, sys
+import os
+import sys
 lib_path = os.path.abspath('../')
 sys.path.append(lib_path)
 
-import ADT, ADTOp
+import ADT
+import ADTOp
+
 
 def findInverse(diagram, op):
     print "findInverse is being called right now."
@@ -11,40 +14,40 @@ def findInverse(diagram, op):
     number = op.getNumber()
     direction = op.getDirection()
     if direction == "U":
-    	reverse = "D"
+        reverse = "D"
     elif direction == "D":
-    	reverse = "U"
+        reverse = "U"
     elif direction == "H":
-    	reverse = "H"
+        reverse = "H"
     if op.apply(K):
-    	print "op.apply(K) seemed to work"
-    	if K.number_crossings() <= 1 and op.getNumber() == 2:
-    		print "Returning not None."
-    		return ADTOp.ADTOp(2, "U")
-    	if K.number_crossings() == 0 and op.getNumber() == 1:
-    		print "Returning not None."
-    		return ADTOp.ADTOp(1, "U")
+        print "op.apply(K) seemed to work"
+        if K.number_crossings() <= 1 and op.getNumber() == 2:
+            print "Returning not None."
+            return ADTOp.ADTOp(2, "U")
+        if K.number_crossings() == 0 and op.getNumber() == 1:
+            print "Returning not None."
+            return ADTOp.ADTOp(1, "U")
         possible_moves = K.finePossibleMoves()
         for move in possible_moves:
-        	print "Checking {} in possible_moves".format(move.toString())
-        	if move.getNumber() == number and move.getDirection() == reverse:
-        		Ktemp = K.copy()
-        		move.apply(Ktemp)
-        		if diagram.sameDiagram(Ktemp):
-#        			print "FOUND IT!"
-#        			return [True, move]
-					print "move is not None, I think."
-					return move
-		print "Done cycling through possible_moves"
-		if op.getNumber() == 2 and op.getDirection() == "D":
-			print "Doing final check to see if we had an 'irreverisble' R2Down move"
-			arc = op.getData()['arc']
-			for  s in ["L", "R"]:
-				for i in [1, -1]:
-					if len(diagram.regions(diagram.wrap(arc + i), s)) == 1:
-						print "Returning not None."
-						return ADTOp.ADTOp('1 & 1', "U")
-			print "Nothing got returned."
+            print "Checking {} in possible_moves".format(move.toString())
+            if move.getNumber() == number and move.getDirection() == reverse:
+                Ktemp = K.copy()
+                move.apply(Ktemp)
+                if diagram.sameDiagram(Ktemp):
+                    #        			print "FOUND IT!"
+                    #        			return [True, move]
+                    print "move is not None, I think."
+                    return move
+            print "Done cycling through possible_moves"
+            if op.getNumber() == 2 and op.getDirection() == "D":
+                print "Doing final check to see if we had an 'irreverisble' R2Down move"
+                arc = op.getData()['arc']
+                for s in ["L", "R"]:
+                    for i in [1, -1]:
+                        if len(diagram.regions(diagram.wrap(arc + i), s)) == 1:
+                            print "Returning not None."
+                            return ADTOp.ADTOp('1 & 1', "U")
+                print "Nothing got returned."
 #         print "*"*30
 #         print "Error!!"
 #         print "n = ", n
@@ -52,8 +55,8 @@ def findInverse(diagram, op):
 #         return [False, diagram.to_list(), n, op.toString()]
 #         raise TypeError("No inverse found.")
     else:
-    	print "About to return None in TestInverses function findInverse"
-    	return None
+        print "About to return None in TestInverses function findInverse"
+        return None
 #    else:
 #    	return None
 #                print "These two moves are inverses."
@@ -64,8 +67,8 @@ def findInverse(diagram, op):
 #    	print "This move does nothing to this diagram."
 #    	return op
 #		return None
-    	
+
 ##L = ADT.ADT([-12, 14, -16, -22, -20, -6, 2, -4, -18, -10, 8], [-1, 1, -1, 1, 1, -1, 1, -1, 1, 1, -1])
-##for i in range(100):
+# for i in range(100):
 ##    M = ADTOp.fineRandomOp(L)
 ##    findInverse(L, M)
