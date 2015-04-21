b62b523665e5576e06af92f4a633e45cff5906cf
Rathbun <mrathbun@d-mhslc-35x-169.fullerton.edu>
2015-04-21 11:04:45 -0700

    Made testSimplfication function.

--- Python/Testing/TestSimplification.py
+++ Python/Testing/TestSimplification.py
@@ -1,16 +1,16 @@
 import os, sys
 lib_path = os.path.abspath('../')
 sys.path.append(lib_path)
-import ADT, ADTOpPopulation, ADTOpPopulationSets
+import ADT, ADTOp, ADTOpPopulation, ADTOpPopulationSets
 from datetime import datetime
 
 print "Starting script."
 start = datetime.now()
 
 
-K = ADT.ADT([-16, -4, -6, -26, -10, -20, 24, 2, 14, 18, 22, 12, 8], [1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1])
+#K = ADT.ADT([-16, -4, -6, -26, -10, -20, 24, 2, 14, 18, 22, 12, 8], [1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1])
 
-def fit(ol):
+def fit(ol, K=ADT.ADT([],[])):
 #    print "Starting fit function." 
 #    startfit = datetime.now()
     L = K.copy()
@@ -23,40 +23,61 @@ def fit(ol):
     return 1.0 + bonus/(d.number_crossings()**2.0 + min_ol.length() + 1.0)
     #return 1.0 + bonus/(d.number_crossings()**5.0 + ccCount + 1.0)
 
-pop = ADTOpPopulationSets.Population(25,30,5, opPopulationType='Move', model='randtail')
+def testSimplification(attempts, length):
+    successes = 0
+    for j in range(attempts):
+        print "Attempt {} of {}".format(j+1, attempts)
+        print "\n"
+        K = ADT.ADT([],[])
+        for k in range(length):
+            M = ADTOp.coarseRandomMove(upBias=3)
+            M.apply(K)
+        print "K is ", K.to_string()
+        print "\n"
+        
+        pop = ADTOpPopulationSets.Population(10,30,5, opPopulationType='Move', model='randtail')
 
-print "pop.size() = %d\n" % (pop.size())
+#        print "pop.size() = %d\n" % (pop.size())
 
-pl = pop.toList()
-for l in pl:
-    print [op.toString() for op in l.toList()]
+        pl = pop.toList()
+#        for l in pl:
+#            print [op.toString() for op in l.toList()]
 
-numiterations = 10
+        numiterations = 20
 
-for i in range(0,numiterations):
-    print "Iteration {} of {}".format(i, numiterations)
-    best = pop.iterate(fit)
-    for l in pop.toList():
-        print [op.toString() for op in l.toList()]
+        for i in range(0,numiterations):
+            print "Iteration {} of {}".format(i, numiterations)
+            best = pop.iterate(fit)
+#            for l in pop.toList():
+#                print [op.toString() for op in l.toList()]
 
-print "***************************************"
-print "\n"
-print "\n"
-pl = pop.toList()
-for l in pl:
-    print [op.toString() for op in l.toList()]
+        print "***************************************"
+        print "\n"
+        print "\n"
+        pl = pop.toList()
+        for l in pl:
+            print [op.toString() for op in l.toList()]
 
-print "\n"
-print "\n"
-print "The best sequence we found is:"
-print [op.toString() for op in best.toList()]
-print "It has fitness {}".format(fit(best))
-L = K.copy()
-d, min_ol = best.apply(L)
-print "The resulting diagram is:"
-print d.to_string()
-print "It has {} crossings.".format(d.number_crossings())
-print "The (effective) length of the sequence is {}".format(len(min_ol.toList()))
+        print "\n"
+        print "\n"
+        print "The best sequence we found is:"
+        print [op.toString() for op in best.toList()]
+        print "It has fitness {}".format(fit(best))
+        L = K.copy()
+        d, min_ol = best.apply(L)
+        print "The resulting diagram is:"
+        print d.to_string()
+        print "It has {} crossings.".format(d.number_crossings())
+        print "The (effective) length of the sequence is {}".format(len(min_ol.toList()))
+        print "\n"
+        if d.number_crossings() < 3:
+            successes += 1
+        print "{} successes so far, out of {} attempts.".format(successes, j)
+        print "\n"
+    
+    print "We had {}% success!".format(float(successes)/float(attempts)*100)
+    
+testSimplification(10, 10)
 
 
 print "Finished script. Took: ", datetime.now() - start
