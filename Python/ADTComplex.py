857850ca646cf2cfc8c76f1b303ccecd87da9775
Nicholas Jackson <dr.nicholas.jackson@gmail.com>
2015-10-15 16:41:32 +0100

    Finished debugging flower calculation code.

--- Python/ADTComplex.py
+++ Python/ADTComplex.py
@@ -6,6 +6,7 @@ class ADTComplex:
         self.n = adt.number_crossings()
         self.n_arcs = 2*self.n
         self.n_regs = self.n + 2
+        self.n_nodes = 4*self.n + 2
         self.inf_reg = 0
         
         self.init_incidence()
@@ -26,11 +27,11 @@ class ADTComplex:
         print "nc = %d" % (self.n)
         print "nr = %d" % (self.n_regs)
         print "np = %d" % (self.n_arcs)
-        self.printmatrix("orientations", [[self.orient(i) for i in range(1,self.n_arcs+1)]])
-        self.printmatrix("f", [[self.f(i) for i in range(1,self.n_arcs+1)]])
-        self.printmatrix("g", [[self.g(i) for i in range(1,self.n_arcs+1)]])
-        self.printmatrix("sign", [[self.sign(i) for i in range(1,self.n_arcs+1)]])
-        self.printmatrix("phi", [[self.adt.phi(i,r) for i in range(1,self.n_arcs+1)] for r in range(self.n_arcs+1)])
+        # self.printmatrix("orientations", [[self.orient(i) for i in range(1,self.n_arcs+1)]])
+        # self.printmatrix("f", [[self.f(i) for i in range(1,self.n_arcs+1)]])
+        # self.printmatrix("g", [[self.g(i) for i in range(1,self.n_arcs+1)]])
+        # self.printmatrix("sign", [[self.sign(i) for i in range(1,self.n_arcs+1)]])
+        # self.printmatrix("phi", [[self.adt.phi(i,r) for i in range(1,self.n_arcs+1)] for r in range(self.n_arcs+1)])
         self.calc_incidence_c2()
         self.calc_incidence_c3()
         self.calc_incidence_er()
@@ -85,7 +86,7 @@ class ADTComplex:
                         b1[k][0] = 1
                     else:
                         b1[k][1] = 1
-        self.printmatrix("c2", self.c2)
+        # self.printmatrix("c2", self.c2)
         return
 
     # C3: Incidence of regions to crossings.
@@ -106,7 +107,7 @@ class ADTComplex:
                         self.c3[j][3] = i
                     if c2[i][adt.wrap(adt.jump(j)-1)] == 1:
                         self.c3[j][4] = i
-        self.printmatrix("c3", self.c3)
+        # self.printmatrix("c3", self.c3)
         return
     
     # ER: Incidence of edges to regions.
@@ -119,7 +120,7 @@ class ADTComplex:
                     if self.c2[reg1][j] == 1:
                         self.er[i][j] = reg1
                         self.er[j][i] = reg1
-        self.printmatrix("er", self.er)
+        # self.printmatrix("er", self.er)
         return
 
     # C4: Edges meeting at crossing.
@@ -131,7 +132,7 @@ class ADTComplex:
             self.c4[i][adt.wrap(adt.jump(i)-1)] = 1
             self.c4[i][adt.jump(adt.wrap(i+1))] = 1
             self.c4[i][adt.wrap(adt.jump(adt.wrap(i+1))-1)] = 1
-        self.printmatrix("c4", self.c4)
+        # self.printmatrix("c4", self.c4)
         return
     
     # RM: Edges shared by adjacent regions
@@ -143,7 +144,7 @@ class ADTComplex:
             self.rm[c3[i][2]][c3[i][1]] = i
             self.rm[c3[i][2]][c3[i][3]] = adt.jump(i)
             self.rm[c3[i][3]][c3[i][2]] = adt.jump(i)
-        self.printmatrix("rm", self.rm)
+        # self.printmatrix("rm", self.rm)
         return
     
     def calc_incidence_kf(self):
@@ -156,7 +157,7 @@ class ADTComplex:
             self.kf[c3[i][2]][i] = f * g
             self.kf[c3[i][3]][i] = -f * g
             self.kf[c3[i][4]][i] = f * g
-        self.printmatrix("kf", self.kf)
+        # self.printmatrix("kf", self.kf)
         return
     
     def calc_incidence_opp(self):
@@ -166,16 +167,17 @@ class ADTComplex:
                 if (rm[i][j] != 0):
                     self.opp[i][rm[i][j]] = j
                     self.opp[j][rm[i][j]] = i
-        self.printmatrix("opp", self.opp)
+        # self.printmatrix("opp", self.opp)
         return
     
     def init_neighbours(self):
-        self.done = [0 for i in range(2*self.n+1)]
+        self.done = [0 for i in range(self.n_nodes+1)]
         self.ccr = [[0] for i in range(self.n_regs+1)]
         self.rot1 = [[0 for j in range(self.n_arcs+1)] for i in range(self.n_regs+1)]
         self.rot2 = [[0 for j in range(self.n_arcs+1)] for i in range(self.n_regs+1)]
         self.newreg = [0 for i in range(self.n_regs + 1)]
-        self.nb = [[0] for i in range(self.n+1)]
+        self.nb = [[0] for i in range(self.n_nodes + 1)]
+        self.twoedge = [0 for i in range(self.n_arcs+1)]
         return
     
     def eb(self,i,j):
@@ -226,25 +228,23 @@ class ADTComplex:
         adt = self.adt
         for reg in range(1,self.n_regs+1):
             cedge = 0
-            #print "c2[%d][%d] = %d" % (reg,cedge,c2[reg][cedge])
             while (c2[reg][cedge] == 0):
-                #print "c2[%d][%d] = %d" % (reg,cedge,c2[reg][cedge])
                 cedge += 1
             fedge = cedge
             self.ccr[reg].append(cedge)
-            dir = self.sign(cedge) * kf[reg][cedge]
+            dir = self.g(cedge) * kf[reg][cedge]
             while True:
-                #print "eb(%d,%d)" % (cedge,dir)
                 node = self.eb(cedge,dir)
                 if (c2[reg][adt.jump(node)] == 1):
                     cedge = adt.jump(node)
                 else:
                     cedge = adt.wrap(adt.jump(node) - 1)
                 self.ccr[reg].append(cedge)
-                dir = self.sign(cedge) * kf[reg][cedge]
+                dir = self.g(cedge) * kf[reg][cedge]
                 if (cedge == fedge):
                     break
-            self.ccr[reg][0] = length(self.ccr[reg]) - 1
+            self.ccr[reg][0] = len(self.ccr[reg]) - 2
+        # self.printmatrix("ccr", self.ccr,1,0)
         return
     
     def calc_neighbours_rot(self):
@@ -253,8 +253,8 @@ class ADTComplex:
             for i in range(1,ccr[reg][0]+1):
                 edge1 = ccr[reg][i]
                 edge2 = ccr[reg][i+1]
-                rot1[reg][edge1] = edge2
-                rot2[reg][edge2] = edge1
+                self.rot1[reg][edge1] = edge2
+                self.rot2[reg][edge2] = edge1
         return
     
     def calc_infinite_region(self):
@@ -264,14 +264,17 @@ class ADTComplex:
             if (ccr[reg][0] > nedge):
                 self.inf_reg = reg
                 nedge = ccr[reg][0]
+        # print "inf_reg = %d" % (self.inf_reg)
     
     def renumber_regions(self):
+        # self.printmatrix("ccr", self.ccr,1,0)
         t = 0
         ccr = self.ccr
         for reg in range(1,self.n_regs+1):
-            if reg != self.inf_reg and ccr[reg][0] != 2:
+            if (reg != self.inf_reg) and (ccr[reg][0] != 2):
                 t += 1
                 self.newreg[reg] = t
+                # print "newreg[%d] = %d" % (reg,t)
         self.n_nodes = 3 * self.n + t + ccr[self.inf_reg][0]
         self.n_intnodes = 3 * self.n + t
         for reg in range(1,self.n_regs+1):
@@ -300,26 +303,28 @@ class ADTComplex:
         nedge = ccr[inf][0]
         for i in range(1,nedge+1):
             cedge = ccr[inf][i]
-            dir = self.sign(cedge) * kf[inf][cedge]
+            dir = self.g(cedge) * kf[inf][cedge]
             prevedge = rot2[inf][cedge]
-            prevdir = self.sign(prevedge) * kf[inf][prevedge]
+            prevdir = self.g(prevedge) * kf[inf][prevedge]
             cb = self.eb(cedge,-dir)
             ivertex.append(self.vertex_index(cb))
             node = self.vertex_index(cb)
             done[node] = 1
             
-            nb[node][0] = [5,0,0,0,0,0]
+            # print "prevedge = %d, prevdir = %d" % (prevedge,prevdir)
+            nb[node] = [5,0,0,0,0,0,0]
             nb[node][1] = n + cedge
             nb[node][2] = n + adt.wrap(prevedge + prevdir)
             nb[node][3] = n + self.be(cb,-dir)
             nb[node][4] = n + prevedge
             nb[node][5] = n_intnodes + i
+            # print "nb[%d] = " % (node), nb[node]
         
         for i in range(1,nedge+1):
             cedge = ccr[inf][i]
             node = n + cedge
             done[node] = 1
-            dir = self.sign(cedge) * kf[inf][cedge]
+            dir = self.g(cedge) * kf[inf][cedge]
             reg2 = opp[inf][cedge]
             if i == nedge:
                 nbleft = n_intnodes + 1
@@ -327,14 +332,15 @@ class ADTComplex:
                 nbleft = n_intnodes + i + 1
             
             if ccr[reg2][0] == 2:
-                nb[node] = [5,0,0,0,0,0]
+                nb[node] = [5,0,0,0,0,0,0]
                 nb[node][1] = nbleft
                 nb[node][3-dir] = self.vertex_index(adt.wrap(cedge+1))
                 nb[node][3] = n + rot1[reg2][cedge]
                 nb[node][3+dir] = self.vertex_index(cedge)
                 nb[node][5] = n_intnodes + i
+                # print "nb[%d] = " % (node), nb[node]
             else:
-                nb[node] = [7,0,0,0,0,0,0,0]
+                nb[node] = [7,0,0,0,0,0,0,0,0]
                 nb[node][1] = nbleft
                 nb[node][4-2*dir] = self.vertex_index(adt.wrap(cedge+1))
                 nb[node][3] = n + rot2[reg2][cedge]
@@ -342,27 +348,30 @@ class ADTComplex:
                 nb[node][5] = n + rot1[reg2][cedge]
                 nb[node][4+2*dir] = self.vertex_index(cedge)
                 nb[node][7] = n_intnodes + i
+                # print "nb[%d] = " % (node), nb[node]
                 
         for i in range(1,nedge+1):
             node = n_intnodes + i
             previdx = (nedge if i == 1 else i - 1)
             nextidx = (1 if i == nedge else i + 1)
             
-            nb[node] = [5,0,0,0,0,0]
+            nb[node] = [4,0,0,0,0,0]
             nb[node][1] = n_intnodes + nextidx
             nb[node][2] = n + ccr[inf][i]
             nb[node][3] = ivertex[i]
             nb[node][4] = n + ccr[inf][previdx]
             nb[node][5] = n_intnodes + previdx
+            # print "nb[%d] = " % (node), nb[node]
         
         for i in range(1,n+1,2):
             if done[self.vertex_index(i)] == 0:
                 node = self.vertex_index(i)
-                nb[node] = [4,0,0,0,0]
+                nb[node] = [4,0,0,0,0,0]
                 nb[node][1] = n + i
                 nb[node][3+self.orient(i)] = n + adt.jump(i)
                 nb[node][3] = n + adt.wrap(i-1)
                 nb[node][3-self.orient(i)] = n + adt.wrap(adt.jump(i) - 1)
+                # print "nb[%d] = " % (node), nb[node]
         
         for cedge in range(1,n_arcs + 1):
             node = n + cedge
@@ -374,10 +383,10 @@ class ADTComplex:
                 
                 if ccr[reg1][0] == 2:
                     reg1,reg2 = reg2,reg1
-                dir = self.sign(cedge) * kf[reg1][cedge]
+                dir = self.g(cedge) * kf[reg1][cedge]
                 
                 if ccr[reg2][0] == 2:
-                    nb[node] = [6,0,0,0,0,0,0]
+                    nb[node] = [6,0,0,0,0,0,0,0]
                     nb[node][1] = n + rot1[reg1][cedge]
                     nb[node][3-dir] = self.vertex_index(adt.wrap(cedge+1))
                     nb[node][3] = n + rot2[reg2][cedge]
@@ -385,33 +394,45 @@ class ADTComplex:
                     nb[node][5] = n + rot2[reg1][cedge]
                     nb[node][6] = 3 * n + newreg[reg1]
                 else:
-                    nb[node] = [8,0,0,0,0,0,0,0,0]
+                    nb[node] = [8,0,0,0,0,0,0,0,0,0]
                     nb[node][1] = n + rot1[reg1][cedge]
                     nb[node][4-2*dir] = self.vertex_index(adt.wrap(cedge+1))
                     nb[node][3] = n + rot2[reg2][cedge]
                     nb[node][4] = 3 * n + newreg[reg2]
                     nb[node][5] = n + rot1[reg2][cedge]
-                    nb[node][4+2*dir] = self.vertex(cedge)
+                    nb[node][4+2*dir] = self.vertex_index(cedge)
                     nb[node][7] = n + rot2[reg1][cedge]
                     nb[node][8] = 3 * n + newreg[reg1]
+                # print "nb[%d] = " % (node), nb[node]
         
-        for reg in range(1,n_regs + 1):
+        for reg in range(1,self.n_regs + 1):
             if reg != inf and ccr[reg][0] != 2:
                 node = 3 * n + newreg[reg]
                 nedge = ccr[reg][0]
-                nb[node][0] = nedge
+                nb[node] = [nedge] + [0 for i in range(nedge+1)]
                 for i in range(1,nedge+1):
+                    # print "nb[%d] = " % (node), nb[node]
                     nb[node][nedge+1-i] = n + ccr[reg][i]
-        for i in range(1,n_nodes+1):
+        
+        print ""
+        
+        for i in range(1,self.n_nodes+1):
             valence = nb[i][0]
             if nb[i][valence+1] == 0:
                 nb[i][valence+1] = nb[i][1]
+            # print "nb[%d]" % (i), nb[i]
         return
     
-    def printmatrix(self,name,m):
+    def flower(self,i):
+        return self.nb[i][1:]
+    
+    def valency(self,i):
+        return self.nb[i][0]
+    
+    def printmatrix(self,name,m,sr=1,sc=1):
         print name
-        for row in m[1:]:
-            for el in row[1:]:
+        for row in m[sr:]:
+            for el in row[sc:]:
                 print "%2d" % (el),
             print ""
         print ""
--- Python/Testing/TestComplex.py
+++ Python/Testing/TestComplex.py
@@ -4,10 +4,11 @@ sys.path.append(lib_path)
 from ADT import *
 from ADTComplex import *
 #L = ADT([2],[1])
-#L = ADT([4,6,2],[1,1,1])
-L = ADT([6,8,2,4],[-1,1,-1,1])
+L = ADT([4,6,2],[-1,-1,-1])
+#L = ADT([4,6,8,2],[-1,1,-1,1])
+#L = ADT([4,8,10,2,6],[-1,-1,-1,-1,-1])
 K = ADTComplex(L)
-K.draw_graph()
-print K.crossings
-print K.arcs
-print K.regions
+
+print "Nodes: %4d" % (K.n_nodes)
+for i in range(1,K.n_nodes+1):
+    print i, K.valency(i), K.flower(i)    
