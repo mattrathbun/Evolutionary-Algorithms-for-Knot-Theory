4af650deee7d64d34dd066a8de167eac849f8c65
Nicholas Jackson <dr.nicholas.jackson@gmail.com>
2016-03-16 17:01:44 +0000

    Fixed (?) orientation method in ADT.

--- Python/Testing/TestOrientation.py
+++ Python/Testing/TestOrientation.py
@@ -3,6 +3,7 @@ lib_path = os.path.abspath('../')
 sys.path.append(lib_path)
 from ADT import *
 
-K = ADT([4,6,8,2],[0,0,0,0])
+#K = ADT([4,6,8,2],[0,0,0,0])
+K = ADT([4, 8, 10, 2, 12, 6],[0,0,0,0,0,0])
 K.orient(1)
 print "K %s realisable." % ("is" if K.isrealisable() else "isn't")
