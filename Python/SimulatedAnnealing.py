e243c42e78e6f5f3daba0734b9f37e49a6f3b636
colingj <colingj@gmail.com>
2016-06-01 17:41:48 +0100

    Simulated Annealing

--- Python/SimulatedAnnealing.py
+++ Python/SimulatedAnnealing.py
@@ -1,7 +1,7 @@
 #import os, sys
 #lib_path = os.path.abspath('../')
 #sys.path.append(lib_path)
-import ADT, ADTOp, ADTOpList
+import ADT, ADTOp, ADTOpList, AllDiagrams
 
 K = ADT.ADT([4,8,12,2,14,6,10], [1, 1, 1, 1, -1, 1, -1])
 Kname = "7_6"
@@ -11,22 +11,22 @@ print "%s : %s\n" % (Kname, K.to_string())
 AllDiagrams.init()
 
 temperature = 2
-k = 1.0
+k_param = 1.0
 n = 0
 
 while True:
     move = ADTOp.simpleCoarseRandomOp()
     L = K.copy()
     if move.apply(L):
-        wildness = k.number_crossings() - l.number_crossings()
-        energy_change = k.energy() - l.energy()
+        wildness = K.number_crossings() - L.number_crossings()
+        energy_change = K.energy() - L.energy()
         print "wildness: %n"
         if (energy_change<temperature): 
             K = L
         else:
-            if random.random() < (1.0/(1.0+exp(energy_change/k*temperature))):
+            if random.random() < (1.0/(1.0+exp(energy_change/k_param*temperature))):
                 K = L
-    print "%s : %s\n" % (Kname, K.to_string())
-    temperature = temperature/math.log1p(n)
+    print "%s \n" % K.to_string()
+    temperature = temperature/log1p(n)
     n = n+1
     
