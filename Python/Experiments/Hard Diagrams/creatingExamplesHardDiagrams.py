0a80b33da4afc76acfd49df69f9baabe04fa5dbe
Matthew Rathbun <mrathbun@mrathbun-mbpro.local>
2016-12-06 09:16:33 -0800

    Modified creatingExamplesHardDiagrams.py.

--- Python/Experiments/Hard Diagrams/creatingExamplesHardDiagrams.py
+++ Python/Experiments/Hard Diagrams/creatingExamplesHardDiagrams.py
@@ -8,73 +8,119 @@ import random
 
 AllDiagrams.init()
 
+storageFile = "SimpleHardDiagrams"
 
-def createCandidateExample(height=5, drop=1):
+def fetchDownHorizontalMoves(knot):
+    all_possible_moves = knot.finePossibleMoves()
+    return [move for move in all_possible_moves if move.direction in ["D", "H"]]
+    
+def fetchDownMoves(knot):
+    all_possible_moves = knot.finePossibleMoves()
+    return [move for move in all_possible_moves if move.direction in ["D"]]
+    
+def resultDownHorizontalMoves(knot):
+    all_possible_down_or_horizontal_moves = fetchDownHorizontalMoves(knot)
+    results = []
+    for move in all_possible_down_or_horizontal_moves:
+        K = knot.copy()
+        if move.apply(K):
+            results.append(K)
+    return results
 
+def createCandidateExample(height=15, drop=1):
+    hardUnknotCount = 0
     code = random.choice([[2], [-2]])
     orient = random.choice([[1], [-1]])
     K = ADT.ADT(code, orient)
     upSequence = []
-    L = K.copy()
 #    ups = 0
-    while L.number_crossings() < height:
-        M = ADTOp.simpleCoarseRandomOp(upMoveBias=1, horizontalMoveBias=1, downMoveBias=0, CCBias=0)
+    print "Starting to increase complexity. \n"
+    while K.number_crossings() < height:
+        M = ADTOp.simpleCoarseRandomOp(upMoveBias=2, horizontalMoveBias=3, downMoveBias=1, CCBias=0)
 #        if M.getDirection() == "U":
 #            ups += 1
 #            print "ups is {}".format(ups)
 #        upSequence.append(M)
-        print "Applying {} to {}.".format(M.toString(), L.to_string())
-        M.apply(L)
-        print "K has become: ", L.to_string()
-    print "K has been built up, and now has {} crossings.".format(L.number_crossings())
+#        print "Applying {} to {}.".format(M.toString(), L.to_string())
+        M.apply(K)
+        print "K has {} crossings. \n".format(K.number_crossings())
+#        print "K has become: ", L.to_string()
+#    print "K has been built up, and now has {} crossings.".format(L.number_crossings())
     dropped = 0
-    all_possible_moves = L.finePossibleMoves()
-    all_possible_down_or_horizontal_moves = [move for move in all_possible_moves if move.direction in ["D", "H"]]
-    while dropped < drop and len(all_possible_down_or_horizontal_moves) > 0:
-        simple_possible_down_or_horizontal_moves = [move for move in L.simpleFinePossibleMoves() if move.direction in ["D", "H"]]
-        while len(simple_possible_down_or_horizontal_moves) == 0:
-            L.shiftLabel()
-            simple_possible_down_or_horizontal_moves = [move for move in L.simpleFinePossibleMoves() if move.direction in ["D", "H"]]
-        M = random.choice(simple_possible_down_or_horizontal_moves)
-        print "Applying {} to {}.".format(M.toString(), L.to_string())
-        M.apply(L)
-        if M.getDirection() == "D":
-            dropped += 1
-            print "drops is {}".format(dropped)
-        print "Now, K has become: ", L.to_string()
-        possible_moves = L.simpleFinePossibleMoves()
-        possible_down_or_horizontal_moves = [move for move in possible_moves if move.direction in ["D", "H"]]
-    print "K has been dropped down, and now has {} crossings.".format(L.number_crossings())
+    L = K.copy()
+    for i in range(1):
+        results = resultDownHorizontalMoves(L)
+        print "There are {} possible reductions in complexity. \n".format(len(results))
+    counter = 1
+    for diag in results:
+        print "Checking {} out of {}.".format(counter, len(results))
+        counter += 1
+        reductions = fetchDownMoves(diag)
+        if len(reductions) == 0:
+            hardUnknotCount += 1
+            myfile = open(storageFile, 'a')
+            myfile.write(str(diag.to_string()))
+            myfile.write('\n')
+            myfile.close()
+        else:
+            print "Has a reduction. \n"
+            print reductions[0].toString()
+    print "Found {} hard unknot diagrams.".format(hardUnknotCount)
 
-    all_possible_moves = L.finePossibleMoves()
-    all_possible_down_or_horizontal_moves = [move for move in all_possible_moves if move.direction in ["D", "H"]]
-    if len(all_possible_down_or_horizontal_moves) > 0:
-        print "This is not a hard diagram."
-        return False, L
-    else:
-        print "THIS IS A HARD DIAGRAM!"
-        print L.to_string()
-        return True, L
+for i in range(5000):
+    p = random.randint(10, 30)
+    print "On iteration {}.\n".format(i)
+    createCandidateExample(height=p, drop=1) 
+        
+#     all_possible_moves = peak.finePossibleMoves()
+#     all_possible_down_or_horizontal_moves = [move for move in all_possible_moves if move.direction in ["D", "H"]]
+#     for move in all_possible_down_or_horizontal_moves:
+#         L
+#     while dropped < drop and len(all_possible_down_or_horizontal_moves) > 0:
+#         simple_possible_down_or_horizontal_moves = [move for move in L.simpleFinePossibleMoves() if move.direction in ["D", "H"]]
+#         while len(simple_possible_down_or_horizontal_moves) == 0:
+#             L.shiftLabel()
+#             simple_possible_down_or_horizontal_moves = [move for move in L.simpleFinePossibleMoves() if move.direction in ["D", "H"]]
+#         M = random.choice(simple_possible_down_or_horizontal_moves)
+#         print "Applying {} to {}.".format(M.toString(), L.to_string())
+#         M.apply(L)
+#         if M.getDirection() == "D":
+#             dropped += 1
+#             print "drops is {}".format(dropped)
+#         print "Now, K has become: ", L.to_string()
+#         possible_moves = L.simpleFinePossibleMoves()
+#         possible_down_or_horizontal_moves = [move for move in possible_moves if move.direction in ["D", "H"]]
+#     print "K has been dropped down, and now has {} crossings.".format(L.number_crossings())
+# 
+#     all_possible_moves = L.finePossibleMoves()
+#     all_possible_down_or_horizontal_moves = [move for move in all_possible_moves if move.direction in ["D", "H"]]
+#     if len(all_possible_down_or_horizontal_moves) > 0:
+#         print "This is not a hard diagram."
+#         return False, L
+#     else:
+#         print "THIS IS A HARD DIAGRAM!"
+#         print L.to_string()
+#         return True, L
 
 
 #storageFile = "CandidateUp"+str(up)+"Down"+str(down)
-storageFile = "SimpleHardDiagrams"
+
  
  
 #height = input("How far up should we climb (in crossings)? ")
 #drop = input("How far down should we drop? ")
 
-while True:
-    p = random.randint(10,30)
-    q = random.randint(2,p/2)
-    success, L = createCandidateExample(p, q)
-    print success
-    print L.to_string()
-    if success and L.number_crossings() > 3:
-        myfile = open(storageFile, 'a')
-        myfile.write(str(L.to_string()))
-        myfile.write('\n')
-        myfile.close()
+# while True:
+#     p = random.randint(10,30)
+#     q = random.randint(2,p/2)
+#     success, L = createCandidateExample(p, q)
+#     print success
+#     print L.to_string()
+#     if success and L.number_crossings() > 3:
+#         myfile = open(storageFile, 'a')
+#         myfile.write(str(L.to_string()))
+#         myfile.write('\n')
+#         myfile.close()
 
 
 
